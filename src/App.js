4f22790c86d088ca1f55025f757ab15ea39b1f7c
Nick Troccoli <nicktroccoli28@gmail.com>
2017-09-09 23:34:58 -0400

    Test update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to Nick's website!</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          (For development) To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
